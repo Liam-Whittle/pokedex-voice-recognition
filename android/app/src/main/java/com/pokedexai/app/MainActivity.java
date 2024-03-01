ea8e894896c748bd11bdbff82a255a462423f09e
Liam Whittle <liamwhittle683@gmail.com>
2024-03-01 19:33:29 +1300

    Fix Java Revert

--- android/app/src/main/java/com/pokedexai/app/MainActivity.java
+++ android/app/src/main/java/com/pokedexai/app/MainActivity.java
@@ -2,9 +2,5 @@ package com.pokedexai.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-
-    WebView v = getBridge().getWebView();
-    v.setOverScrollMode(v.OVER_SCROLL_NEVER);
-}
+public class MainActivity extends BridgeActivity {}
 
