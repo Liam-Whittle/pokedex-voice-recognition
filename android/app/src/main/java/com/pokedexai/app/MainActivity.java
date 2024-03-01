90e6c84b45a88e8a485f0af284bbf94db8dbd401
Liam Whittle <liamwhittle683@gmail.com>
2024-03-01 19:18:23 +1300

    Fix

--- android/app/src/main/java/com/pokedexai/app/MainActivity.java
+++ android/app/src/main/java/com/pokedexai/app/MainActivity.java
@@ -3,3 +3,6 @@ package com.pokedexai.app;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {}
+
+WebView v = getBridge().getWebView();
+v.setOverScrollMode(v.OVER_SCROLL_NEVER);
